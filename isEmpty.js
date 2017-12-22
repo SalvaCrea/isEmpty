508a23f5fff5b7136e531220241570d7e0c3d48b
Cardona <SalvaCrea@users.noreply.github.com>
2017-12-22 11:44:23 +0100

    Create isEmpty.js

---
+++ isEmpty.js
@@ -0,0 +1,29 @@
+/**
+ * Determines if variable is empty
+ * @param  {[mixed]}  variable  Variable to check
+ * @return {Boolean}            return true if empty or false
+ */
+function isEmpty( variable ){
+    function testLength( variable ){
+        return variable.length === 0 ? true : false;
+    }
+    switch( typeof variable) {
+    case 'string':
+        return testLength( variable )
+        break;
+    case 'object':
+        if ( Array.isArray( variable ) ) {
+            return testLength( variable );
+        } else {
+            for (var key in variable) {
+                if (Object.prototype.hasOwnProperty.call(obj, key)) return false;
+            }
+        }
+        break;
+    case 'boolean':
+        return variable;
+        break;
+    default:
+        return true;
+    }
+}
